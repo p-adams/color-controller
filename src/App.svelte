a8975cdcb9c82b1b2a5da504b7e749be322b276c
Paul Adams <ghusiyy@gmail.com>
2023-12-30 00:27:31 -0800

    add color control component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+  import ColorControl from "./lib/ColorControl.svelte";
+  import Counter from "./lib/ColorControl.svelte";
+
   let backgroundColor = { r: 255, g: 0, b: 0 };
   $: color = `rgb(${backgroundColor.r}, ${backgroundColor.g}, ${backgroundColor.b})`;
   $: hex = rgbToHex(backgroundColor.r, backgroundColor.g, backgroundColor.b);
+
   function rgbToHex(r: number, g: number, b: number): string {
     // Ensure that the values are in the valid range [0, 255]
     const clamp = (value: number) => Math.min(255, Math.max(0, value));
@@ -24,56 +28,56 @@
 
   <div class="card">
     <div>
-      {hex}
+      <div class="hex-display">{hex}</div>
     </div>
     <div class="color-square" style="background-color: {color};"></div>
-
-    <label for="red">
-      <input
-        type="range"
-        id="red"
-        name="red"
-        min="0"
-        max="255"
-        bind:value={backgroundColor.r}
-        step="10"
-      />
-      Red {backgroundColor.r}
-    </label>
-
-    <label for="green">
-      <input
-        type="range"
-        id="green"
-        name="green"
-        min="0"
-        max="255"
-        bind:value={backgroundColor.g}
-        step="10"
-      />
-      Green {backgroundColor.g}
-    </label>
-
-    <div>
-      <label for="blue">
-        <input
-          type="range"
-          id="blue"
-          name="blue"
-          min="0"
-          max="255"
-          bind:value={backgroundColor.b}
-          step="10"
-        />
-        Blue {backgroundColor.b}
-      </label>
-    </div>
+    <ColorControl
+      label="Red"
+      hexVal={rgbToHex(backgroundColor.r, 0, 0)}
+      numberVal={backgroundColor.r}
+      on:onColorChange={(e) => (backgroundColor.r = e.detail.value)}
+    />
+    <ColorControl
+      label="Green"
+      hexVal={rgbToHex(0, backgroundColor.g, 0)}
+      numberVal={backgroundColor.g}
+      on:onColorChange={(e) => (backgroundColor.g = e.detail.value)}
+    />
+    <ColorControl
+      label="Blue"
+      hexVal={rgbToHex(0, 0, backgroundColor.b)}
+      numberVal={backgroundColor.b}
+      on:onColorChange={(e) => (backgroundColor.b = e.detail.value)}
+    />
   </div>
 </main>
 
 <style>
+  .hex-display {
+    outline: 1px solid lightgray;
+    font-size: x-large;
+    padding: 14px;
+    border-radius: 4px;
+    background: #3d3d3d;
+    color: #fff;
+  }
   .color-square {
     width: 100px;
     height: 100px;
+    box-shadow: 10px 5px 5px lightgray;
+  }
+  .color-input-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+  }
+  .color-input-wrapper label {
+    display: flex;
+  }
+  .color-sample {
+    width: 55px;
+    height: 25px;
   }
 </style>
---
+++ src/lib/ColorControl.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  let color = "";
+  export let label = "";
+  export let hexVal = "";
+  export let numberVal = 0;
+  const dispatch = createEventDispatcher();
+</script>
+
+<div class="color-input-wrapper">
+  <label for={`color-input-${label}`}>
+    <input
+      type="range"
+      id={`color-input-${label}`}
+      name={`color-input-${label}`}
+      min="0"
+      max="255"
+      bind:value={color}
+      on:input={() => dispatch("onColorChange", { value: color })}
+      step="1"
+    />
+    {label}
+    {numberVal}
+  </label>
+  <div class="color-sample" style="background-color: {hexVal};" />
+</div>
+
+<style>
+  .color-input-wrapper {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+  }
+  .color-input-wrapper label {
+    display: flex;
+  }
+  .color-sample {
+    width: 55px;
+    height: 25px;
+  }
+</style>
